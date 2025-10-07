54edbf1392ac3599961260d46a5d1442a895c68d
adewever <anais.dewever@epfedu.fr>
2025-10-07 14:17:57 +0200

    Design Sign in

--- src/app/pagesWeb/signin/signin.component.ts
+++ src/app/pagesWeb/signin/signin.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'signin',
   templateUrl: './signin.component.html',
-  styleUrl: './signin.component.scss'
+  styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent {
+  pseudo: string = '';
 
+  onSignin() {
+    console.log('Signin clicked with pseudo:', this.pseudo);
+
+  }
 }
