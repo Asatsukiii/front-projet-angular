dea09a405587e8fd093c7a81ba37c1c327d93fe3
adewever <anais.dewever@epfedu.fr>
2025-10-13 10:11:49 +0200

    Design page classement avec dummy data

--- src/app/pagesWeb/classement/classement.component.ts
+++ src/app/pagesWeb/classement/classement.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface JoueurClassement {
+  position: number;
+  pseudo: string;
+  partiesJouees: number;
+  partiesGagnees: number;
+  tauxVictoire: number;
+}
+
 @Component({
   selector: 'classement',
   templateUrl: './classement.component.html',
-  styleUrl: './classement.component.scss'
+  styleUrls: ['./classement.component.scss']
 })
 export class ClassementComponent {
 
+  joueurs: JoueurClassement[] = [
+    { position: 1, pseudo: 'Diana', partiesJouees: 10, partiesGagnees: 5, tauxVictoire: 50 },
+    { position: 2, pseudo: 'Alice', partiesJouees: 5, partiesGagnees: 2, tauxVictoire: 40 },
+    { position: 3, pseudo: 'Bob', partiesJouees: 8, partiesGagnees: 3, tauxVictoire: 37.5 },
+    { position: 4, pseudo: 'Charlie', partiesJouees: 2, partiesGagnees: 0, tauxVictoire: 0 }
+  ];
 }
